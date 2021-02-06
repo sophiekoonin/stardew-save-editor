0966d6ddc569ac313648f88cfd406e6a6bdbe919
Sophie Koonin <sophie@localghost.dev>
2021-02-06 15:33:09 +0000

    Switch hosts

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
-const fs = require("fs")
+const fs = require("fs/promises")
 const parser = require("fast-xml-parser")
 const path = require("path")
 const yargs = require("yargs")
-const { help } = require("yargs")
 
-function parseSavefile(pathname) {
+const { switchHost } = require("./lib/player")
+
+const XmlParser = parser.j2xParser
+let filePath
+
+async function overwriteSavefile(data) {
+  const Parser = new XmlParser()
   try {
-    const data = fs.readFileSync(pathname, {
+    const xml = Parser.parse({ SaveGame: data })
+
+    await fs.writeFile(`${filePath}-new`, xml, { encoding: "utf-8" })
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+async function doSwitchHost(argv) {
+  const savedata = await parseSavefile(argv.savefile)
+  const editedSave = switchHost(argv.name, savedata)
+  await overwriteSavefile(editedSave)
+  console.log(`Host changed to ${argv.name}!`)
+}
+
+async function parseSavefile(pathname) {
+  filepath = pathname
+
+  try {
+    await fs.copyFile(pathname, `${pathname}_backup`)
+    const data = await fs.readFile(pathname, {
       encoding: "utf8",
     })
-    return parser.parse(data)
+
+    return parser.parse(data)?.SaveGame
   } catch (err) {
     console.error(err)
   }
@@ -17,16 +43,16 @@ function parseSavefile(pathname) {
 
 yargs
   .command(
-    "load [savefile]",
-    "load in a savefile",
+    "host [name] [savefile]",
+    "Make someone the host",
     (yargs) => {
       yargs.positional("savefile", {
         type: "string",
         describe: "The pathname of your savefile",
       })
     },
-    function (argv) {
-      console.log(parseSavefile(argv.savefile))
+    async function (argv) {
+      doSwitchHost(argv)
     }
   )
   .help().argv
